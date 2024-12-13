ac785572085632f70764992121e5f1b704f9957d
Kuki031 <luka.kukucka1@gmail.com>
2024-12-13 19:53:32 +0100

    flash message style changes

--- flashMessage.php
+++ flashMessage.php
@@ -13,9 +13,9 @@ if (isset($_SESSION['err'])) {
 <div class="flash-wrap">
     <div class="<?= $clazz?>">
         <p><?= $_SESSION['flash']?></p>
-        <button class="flash__close">
+        <div class="flash__close">
             X
-        </button>
+        </div>
     </div>
 </div>
     <?php unset($_SESSION['flash'], $_SESSION['err'], $_SESSION['success']); ?>
